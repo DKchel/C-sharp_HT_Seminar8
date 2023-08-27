c8adaa86ed55296ebee7fd916cccefa6457f92b7
KDA <kuda74ru@gmail.ru>
2023-08-27 20:59:55 +0500

    add hometask02

---
+++ Hometasks/HomeTask02/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите первую размерность");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую размерность");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] matrix = new int[rows, columns];
+
+int sum = 0;
+int sumMin = 999999;
+int rowNum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum += matrix[i, j];
+
+    }
+    if (sum < sumMin)
+    {
+        sumMin = sum;
+        rowNum = i + 1;
+    }
+    sum = 0;
+    Console.WriteLine();
+
+}
+Console.WriteLine("Строка с наименьшей суммой: " + rowNum);
